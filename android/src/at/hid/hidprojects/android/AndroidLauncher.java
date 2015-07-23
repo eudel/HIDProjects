e4227b000b69efefca8a9a9a8fff433992b8bb91
eudel <w.hiertz@gmail.com>
2015-07-23 15:03:21 +0200

    initial commit

---
+++ android/src/at/hid/hidprojects/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package at.hid.hidprojects.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import at.hid.hidprojects.HIDProjects;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HIDProjects(), config);
+	}
+}
---
+++ desktop/src/at/hid/hidprojects/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package at.hid.hidprojects.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import at.hid.hidprojects.HIDProjects;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HIDProjects(), config);
+	}
+}
